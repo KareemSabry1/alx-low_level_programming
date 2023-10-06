dc2f56ac4bedc486685728c1ba6e7979ac76f341
KareemSabry1 <kkareemsabryy@gmail.com>
2023-10-05 18:15:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+/* puts function */
+
+/**
+ * Main: is entry point
+ *
+ * Description: C code that prints with puts
+ *
+ * Return: value is 0 which means it is successful 
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
